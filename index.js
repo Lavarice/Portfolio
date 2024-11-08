c48ccdf3d32fa4363ed1ac8b90c68bacf30ecf8a
Quentin jeandel <lobsteur@gmail.com>
2024-11-08 23:30:09 +0100

    V2.3

--- index.js
+++ index.js
@@ -10,9 +10,34 @@ let progress = setInterval(() => {
 
     progressValue.textContent = `${progressStartValue}%`
 
-    circularProgress.style.background = `conic-gradient(rgb(0, 225, 255) ${progressStartValue * 3.6}deg, #ededed 0deg)`
+    circularProgress.style.background = `conic-gradient(#0CF25D ${progressStartValue * 3.6}deg, #ededed 0deg)`
 
     if(progressStartValue == progressEndValue){
         clearInterval(progress);
     }    
 }, speed);
+
+
+
+// Nombre d'étoiles
+const starCount = 100;
+
+// Création et positionnement des étoiles
+for (let i = 0; i < starCount; i++) {
+  const star = document.createElement("div");
+  star.classList.add("star");
+
+  // Taille aléatoire des étoiles
+  const size = Math.random() * 5 + 2 + "px";
+  star.style.width = size;
+  star.style.height = size;
+
+  // Position aléatoire
+  star.style.top = Math.random() * 100 + "vh";
+  star.style.left = Math.random() * 100 + "vw";
+
+  // Délai d'animation pour un effet de clignotement différent
+  star.style.animationDelay = Math.random() * 3 + "s";
+
+  document.body.appendChild(star);
+}
